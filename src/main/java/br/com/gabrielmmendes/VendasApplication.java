a108607c6316588915a0ce59f17595db0e5b7b3d
Gabriel Mendes <gabrielmendes.0904@gmail.com>
2021-01-18 08:46:38 -0300

    Recomeçando o projeto sem as etapas de aprendizado

--- src/main/java/br/com/gabrielmmendes/VendasApplication.java
+++ src/main/java/br/com/gabrielmmendes/VendasApplication.java
@@ -1,22 +1,12 @@
 package br.com.gabrielmmendes;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
-
-    @Value("${application.name}")
-    private String applicationName;
-
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return applicationName;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
